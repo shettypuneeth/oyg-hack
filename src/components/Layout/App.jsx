d8f12b1c31d54b1f2efe11752de34fdf539e5d9d
pushett <puneeth102@gmail.com>
2017-03-28 16:43:41 +0530

    sanitize

--- src/components/Layout/App.jsx
+++ src/components/Layout/App.jsx
@@ -1,48 +1,11 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
-import Home from '../Home/Home';
-import * as routes from '../../routes';
 import './styles.css';
-import Magic from './magic-meme.jpg';
+
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <div>
-            <ul className='list'>
-              <li>
-                <Link to='/' >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to='/demo'>
-                  Demos
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <hr/>
-
-          <Route exact path="/" component={Home} />
-          <Route path="/demo" component={routes.Demo} />
-          
-          <div>
-            <img
-              className='image'
-              src={Magic}
-              alt=""
-            />
-          </div>
-        </div>
-      </Router>
+      <div>Hello</div>
     );
   }
 }
--- src/routes.js
+++
@@ -1,3 +0,0 @@
-import Async from './components/Async/Async';
-
-export const Demo = Async(() => import('./components/Demo/Demo.jsx').then((module) => module.default));
