4a84a01af9a996ba4f9ec360af0712234b4a1fef
pushett <puneeth102@gmail.com>
2017-03-30 15:54:12 +0530

    Add better grid

---
+++ src/components/Goals/Goals.jsx
@@ -0,0 +1,17 @@
+import React, {Component, PropTypes} from 'react';
+import classnames from 'classnames/bind';
+import styles from './styles.css';
+
+const cx = classnames.bind(styles);
+
+class Goals extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+Goals.propTypes = {};
+
+export default Goals;
