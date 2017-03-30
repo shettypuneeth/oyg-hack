ba2ac20b09a748407178641c0e6fe471229aafb1
Deepak Sharma <dsharm@microsoft.com>
2017-03-30 12:31:41 +0530

    Testing layout

---
+++ src/components/Testing/Testing.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Testing extends Component {
+    render() {
+        return (
+            <div>Hello World</div>
+        );
+    }
+}
+
+Testing.propTypes = {};
+Testing.defaultProps = {};
+
+export default Testing;
